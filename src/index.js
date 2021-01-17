612cfe84c6e5e302fc6a26a76909a249e0adab5f
Tersoo Atsen <tersoo.atsen@outlook.com>
2021-01-17 13:52:03 +0100

    fix over scroll problem

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
-const app = <App />;
-
-ReactDOM.render(app, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
